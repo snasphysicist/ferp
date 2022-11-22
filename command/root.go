2c6087c7cae63739b71a8202b6cfe3366091d1c7
snasphysicist <ih8coldplay@hotmail.com>
2022-11-22 18:49:02 +0000

    feat: main - root command boilerplate

---
+++ command/root.go
@@ -0,0 +1,4 @@
+package command
+
+// Execute is the main entry point for the whole application
+func Execute() {}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/snasphysicist/ferp/v2/command"
+
+func main() {
+	command.Execute()
+}
