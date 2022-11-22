163bfbca8f6a33bae7eb177d86685aefc1812268
snasphysicist <ih8coldplay@hotmail.com>
2022-11-22 18:52:06 +0000

    feat: root command boilerplate

--- command/root.go
+++ command/root.go
@@ -1,4 +1,15 @@
 package command
 
+import "github.com/spf13/cobra"
+
 // Execute is the main entry point for the whole application
-func Execute() {}
+func Execute() {
+	root := cobra.Command{
+		Use:   "ferp",
+		Short: "fabulously easy reverse proxy",
+		Long:  "a super easy to use reverse proxy, that supports http & https incoming, and multiple downstream services",
+	}
+	var path string
+	root.PersistentFlags().StringVar(&path, "configuration-file", "", "path to the proxy configuration file")
+	_ = root.Execute()
+}
