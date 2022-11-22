df54833de3c9307a4c804b98edcbe28ebba3455b
snasphysicist <ih8coldplay@hotmail.com>
2022-11-22 19:09:12 +0000

    feat: initialise logging as the first action

--- command/root.go
+++ command/root.go
@@ -1,9 +1,13 @@
 package command
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/snasphysicist/ferp/v2/pkg/log"
+	"github.com/spf13/cobra"
+)
 
 // Execute is the main entry point for the whole application
 func Execute() {
+	defer initialiseLogging()()
 	root := cobra.Command{
 		Use:   "ferp",
 		Short: "fabulously easy reverse proxy",
@@ -12,4 +16,13 @@ func Execute() {
 	var path string
 	root.PersistentFlags().StringVar(&path, "configuration-file", "", "path to the proxy configuration file")
 	_ = root.Execute()
+}
+
+// initialiseLogging initialises the logging package
+func initialiseLogging() func() {
+	flush, err := log.Initialise()
+	if err != nil {
+		panic(err)
+	}
+	return flush
 }
