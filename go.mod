10f3be2dcdbe55c17dc1fac74aa3c9f3f2d4a486
snasphysicist <ih8coldplay@hotmail.com>
2022-11-22 19:08:57 +0000

    feat: define log functions to be used in the application

--- go.mod
+++ go.mod
@@ -2,9 +2,16 @@ module github.com/snasphysicist/ferp/v2
 
 go 1.19
 
-require github.com/spf13/cobra v1.6.1
+require (
+	github.com/spf13/cobra v1.6.1
+	go.uber.org/zap v1.23.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
 )
---
+++ pkg/log/log.go
@@ -0,0 +1,34 @@
+package log
+
+import (
+	"go.uber.org/zap"
+)
+
+// logger is the global logger instance
+// TODO: avoid package level mutable state
+var logger *zap.SugaredLogger
+
+// Initialise initialises the logger
+func Initialise() (func(), error) {
+	l, err := zap.NewProduction()
+	if err != nil {
+		return func() {}, err
+	}
+	logger = l.Sugar()
+	return func() { _ = l.Sync() }, nil
+}
+
+// Errorf logs an error level message
+func Errorf(t string, args ...interface{}) {
+	logger.Errorf(t, args)
+}
+
+// Infof logs an information level message
+func Infof(t string, args ...interface{}) {
+	logger.Infof(t, args)
+}
+
+// Debugf logs a debug level message
+func Debugf(t string, args ...interface{}) {
+	logger.Debugf(t, args)
+}
