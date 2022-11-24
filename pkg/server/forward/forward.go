240c6450510458e1cdf5eee087b556902cabe005
snasphysicist <ih8coldplay@hotmail.com>
2022-11-24 19:15:24 +0000

    feat: a function which can set up routes for proxied paths

---
+++ pkg/server/forward/forward.go
@@ -0,0 +1,27 @@
+package forward
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/snasphysicist/ferp/v2/pkg/configuration"
+	"github.com/snasphysicist/ferp/v2/pkg/log"
+	"github.com/snasphysicist/ferp/v2/pkg/proxy"
+)
+
+// Configure sets up on the router all proxy routes defined in the incomings
+func Configure(r *chi.Mux, incs []configuration.Incoming) {
+	for _, i := range incs {
+		rm := proxy.Remapper{
+			Protocol: i.Downstream.Protocol,
+			Host:     i.Downstream.Host,
+			Port:     i.Downstream.Port,
+			Base:     i.Downstream.Base,
+			Mapper:   i.Downstream.Mapper.Map,
+		}
+		log.Infof("For Incoming %#v constructed Remapper %#v ", i, rm)
+		for _, mr := range i.MethodRouters {
+			log.Infof("Configuring remapper %#v for incoming '%s' with %#v",
+				rm, i.Path, mr)
+			mr.Route(r, i.Path, rm.ForwardRequest)
+		}
+	}
+}
