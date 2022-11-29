9b683e1868d5fc23d85fa3ebafcbca28e9e4ba91
snasphysicist <ih8coldplay@hotmail.com>
2022-11-29 19:54:10 +0000

    feat: add a test for routing to two different downstreams

--- test/integration/forward_configured_unchanged_routes_test.go
+++ test/integration/forward_configured_unchanged_routes_test.go
@@ -45,3 +45,60 @@ func TestForwardsOnConfiguredExactRouteAndMethod(t *testing.T) {
 		t.Errorf("Forwarded content %s != sent content %s", string(b), content)
 	}
 }
+
+func TestForwardsPathsToCorrespondingDownstreams(t *testing.T) {
+	content1 := "Reached the test route"
+	m1 := mock{t: t, port: mockPorts()[0], routes: []route{
+		{path: "/test", method: http.MethodGet, code: 200, content: content1},
+	}}
+	shutdown := m1.start()
+	defer shutdown()
+
+	content2 := "Reached the other test route"
+	m2 := mock{t: t, port: mockPorts()[1], routes: []route{
+		{path: "/other/test", method: http.MethodGet, code: 200, content: content2},
+	}}
+	shutdown = m2.start()
+	defer shutdown()
+
+	c, err := configuration.Load(mustFindFile("test.yaml", "."))
+	if err != nil {
+		panic(err)
+	}
+
+	stop := make(chan struct{})
+	defer close(stop)
+	go command.Serve(c, stop)
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:23443/test", http.NoBody)
+	if err != nil {
+		panic(err)
+	}
+	res := doUntilResponse(req, 11, time.Millisecond)
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("Request failed with %d, but should have been forwarded", res.StatusCode)
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	if string(b) != content1 {
+		t.Errorf("Forwarded content %s != sent content %s", string(b), content1)
+	}
+
+	req2, err := http.NewRequest(http.MethodGet, "http://localhost:23443/other/test", http.NoBody)
+	if err != nil {
+		panic(err)
+	}
+	res2 := doUntilResponse(req2, 11, time.Millisecond)
+	if res2.StatusCode != http.StatusOK {
+		t.Errorf("Request failed with %d, but should have been forwarded", res2.StatusCode)
+	}
+	b, err = io.ReadAll(res2.Body)
+	if err != nil {
+		panic(err)
+	}
+	if string(b) != content2 {
+		t.Errorf("Forwarded content %s != sent content %s", string(b), content2)
+	}
+}
