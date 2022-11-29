92a1258ef135dcccef7dc55ee7e4b5e4a7e62054
snasphysicist <ih8coldplay@hotmail.com>
2022-11-29 18:53:18 +0000

    feat: create the HTTPS proxy server

---
+++ pkg/server/https.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/snasphysicist/ferp/v2/pkg/configuration"
+	"github.com/snasphysicist/ferp/v2/pkg/server/forward"
+	"github.com/snasphysicist/ferp/v2/pkg/server/redirect"
+)
+
+// HTTPS sets up the HTTPS proxy server, returning it ready to serve
+func HTTPS(c configuration.HTTPS) *http.Server {
+	r := chi.NewRouter() // TODO: add logging middleware
+	redirect.Configure(r, c.Redirects)
+	forward.Configure(r, c.Incoming)
+	return &http.Server{Addr: fmt.Sprintf("0.0.0.0:%d", c.Port), Handler: r}
+}
