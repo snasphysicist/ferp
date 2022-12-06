e94d108aeff065a632e97669047c20ae6f274c3a
snasphysicist <ih8coldplay@hotmail.com>
2022-12-06 19:02:59 +0000

    feat: capture the response code and the number of bytes written

--- pkg/middleware/logging.go
+++ pkg/middleware/logging.go
@@ -27,6 +27,37 @@ func logging(next http.Handler) http.Handler {
 func logRequest(w http.ResponseWriter, r *http.Request, next http.Handler) {
 	start := time.Now()
 	log.Infof("%s request to %s", r.Method, r.URL.String())
-	next.ServeHTTP(w, r)
-	log.Infof("Took %s", time.Since(start))
+	rr := wrap(w)
+	next.ServeHTTP(&rr, r)
+	log.Infof("Responded %d, wrote %d, took %s", rr.code, rr.written, time.Since(start))
+}
+
+// responseRecorder records some properties of the response which we want to log
+type responseRecorder struct {
+	code    int
+	written int
+	w       http.ResponseWriter
+}
+
+// wrap wraps the provided writer in a recorder
+func wrap(w http.ResponseWriter) responseRecorder {
+	return responseRecorder{w: w}
+}
+
+// WriteHeader captures the status code and forwards it to the wrapper writer
+func (w *responseRecorder) WriteHeader(status int) {
+	w.code = status
+	w.w.WriteHeader(status)
+}
+
+// Write writes the bytes to the wrapper writer and records the number of bytes written
+func (w *responseRecorder) Write(b []byte) (int, error) {
+	n, err := w.w.Write(b)
+	w.written = n
+	return n, err
+}
+
+// Header just forwards to the wrapped writer's Header method
+func (w *responseRecorder) Header() http.Header {
+	return w.w.Header()
 }
