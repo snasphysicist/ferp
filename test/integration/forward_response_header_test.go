c633a0275bf56563b2d054122e8fe937b151c1b1
snasphysicist <ih8coldplay@hotmail.com>
2023-04-13 19:30:40 +0100

    feat: test that allowed headers are always forwarded

---
+++ test/integration/forward_response_header_test.go
@@ -0,0 +1,59 @@
+package integration
+
+import (
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestForwardAllResponseHeadersExceptDroppedOnes(t *testing.T) {
+	m := mock{t: t, port: mockPorts()[0], routes: []route{
+		{path: "/test", method: http.MethodGet, rg: withSetHeaders()},
+	}}
+
+	p, f := startMocksAndProxy(t, []mock{m})
+	defer f()
+
+	// The bug this catches happens intermittently, due to randomised map order
+	for i := 0; i < 100; i++ {
+		sendRequestExpectResponse(t, requestResponse{
+			req: request{
+				method:  http.MethodGet,
+				url:     proxyURL(p, "test"),
+				body:    http.NoBody,
+				headers: make(http.Header),
+			},
+			res: response{
+				code:    http.StatusOK,
+				content: stringMatch{expect: ""},
+				headers: checkContainsSetHeaders{},
+			},
+		})
+	}
+}
+
+// withSetHeaders generates a response with 200 status, empty body, and some known headers.
+// One of the headers should not be forwarded along by the proxy, one of them should.
+func withSetHeaders() responseGenerator {
+	return func(_ *http.Request) responseSpecification {
+		return responseSpecification{status: 200, body: "", headers: http.Header{
+			"Connection":   []string{"Shouldn't-Forward"},
+			"Content-Type": []string{"application/json"},
+		}}
+	}
+}
+
+type checkContainsSetHeaders struct{}
+
+func (checkContainsSetHeaders) Check(t *testing.T, h http.Header) {
+	if _, ok := h["Connection"]; ok {
+		t.Fatalf("Connection header was forwarded in %#v", h)
+	}
+	vs, ok := h["Content-Type"]
+	if !ok {
+		t.Fatalf("Content-Type header was not forwarded in %#v", h)
+	}
+	if !reflect.DeepEqual(vs, []string{"application/json"}) {
+		t.Fatalf("Content-Type values %v, expected one value application/json", vs)
+	}
+}
